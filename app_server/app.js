e03549b2c25fe5f6c4834ef6e8385ba50d957fea
Daniel Lip <116948520+gxc1998github@users.noreply.github.com>
2023-08-11 11:59:00 +1200

    add components

--- app_server/app.js
+++ app_server/app.js
@@ -1,14 +1,28 @@
-//app.js
+// app.js
 
 const express = require('express');
 const connectDB = require('./config/db');
+const cors = require('cors');
+
+// routes
+const books = require('./routes/api/books');
 
 const app = express();
 
-//connect database
+// Connect Database
 connectDB();
 
-app.get('/', (req,res) => res.send('Hello world!'));
-const port = process.env.PORT || 8082;
-app.listen(port, () => console.log('Server running on port ${port}'));
+// cors
+app.use(cors({ origin: true, credentials: true }));
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.send('Hello world!'));
+
+// use Routes
+app.use('/api/books', books);
+
+const port = process.env.PORT || 8082;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
